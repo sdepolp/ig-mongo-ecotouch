2eb9e7aa5ce4b46fe3d9e06583311931b375b223
Santiago De Pol <sdepolp@gmail.com>
2023-07-19 00:30:29 -0400

    FIX | Controlar Excepciones

--- src/main/java/cl/ecotouch/igmongo/controller/PickupController.java
+++ src/main/java/cl/ecotouch/igmongo/controller/PickupController.java
@@ -4,8 +4,10 @@ import cl.ecotouch.igmongo.dto.RequestPickupDTO;
 import cl.ecotouch.igmongo.service.PickupService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,12 +25,24 @@ public class PickupController {
     @CrossOrigin("*")
     @GetMapping(value="/get-all-orders")
     public ResponseEntity<?> getAllOrders(){
-        return ResponseEntity.ok(pickupService.getAllOrders());
+        try{
+            return ResponseEntity.ok(pickupService.getAllOrders());
+        }catch (HttpStatusCodeException ex) {
+            return ResponseEntity.status(ex.getStatusCode()).contentType(MediaType.APPLICATION_JSON).body(ex.getResponseBodyAsString());
+        }catch (Exception exception) {
+            return ResponseEntity.internalServerError().contentType(MediaType.APPLICATION_JSON).body(exception.getMessage());
+        }
     }
 
     @CrossOrigin("*")
     @GetMapping(value="/get-daily-orders")
     public ResponseEntity<?> getDailyOrders(){
-        return ResponseEntity.ok(pickupService.getDailyOrders());
+        try{
+            return ResponseEntity.ok(pickupService.getDailyOrders());
+        }catch (HttpStatusCodeException ex) {
+            return ResponseEntity.status(ex.getStatusCode()).contentType(MediaType.APPLICATION_JSON).body(ex.getResponseBodyAsString());
+        }catch (Exception exception) {
+            return ResponseEntity.internalServerError().contentType(MediaType.APPLICATION_JSON).body(exception.getMessage());
+        }
     }
 }
